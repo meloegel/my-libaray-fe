778f97dc10434d96fd250d5b461194380117f44b
meloegel <meloegel@gmail.com>
2020-12-11 18:53:17 -0500

    updated user profile with react bootstrap

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -2,10 +2,16 @@ import React, { useState, useEffect, useContext } from 'react'
 import { useHistory } from "react-router-dom";
 import { axiosWithAuth } from '../../utils/axiosWithAuth'
 import UserContext from '../../contexts/UserContext'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
 
 const initialDetails = {
     username: '',
-    email: ''
+    email: '',
+    avatar: ''
 }
 
 const UserProfile = () => {
@@ -47,27 +53,52 @@ const UserProfile = () => {
 
     return (
         <div>
-            <h2 id='updateUserInfo'>Update Account Information</h2>
-            <form id='userInfoForm' onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="username"
-                    onChange={handleChange}
-                    placeholder="Username"
-                    value={details.username}
-                />
-                <div />
+            <Card body bg='success' className='formCardAcc'>
+                <h2 id='updateUserInfo'>Update Account Information</h2>
+                <form className='form'>
+                    <InputGroup id='input'>
+                        <FormControl
+                            placeholder="Username"
+                            aria-label="Username"
+                            aria-describedby="basic-addon1"
+                            value={details.username}
+                            onChange={handleChange}
+                            name="username"
+                            type="text"
+                        />
+                    </InputGroup>
+                    <InputGroup id='input'>
+                        <FormControl
+                            placeholder="Email"
+                            aria-label="Email"
+                            aria-describedby="basic-addon1"
+                            value={details.email}
+                            onChange={handleChange}
+                            name="email"
+                            type="email"
+                        />
+                    </InputGroup>
+                    <InputGroup id='input'>
+                        <InputGroup.Prepend className='inputPrepend'>
+                            <InputGroup.Text id="basic-addon3">
+                                https://
+                                        </InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                            placeholder="Avatar"
+                            aria-label="Avatar"
+                            aria-describedby="basic-addon1"
+                            value={details.avatar}
+                            onChange={handleChange}
+                            name="avatar"
+                            type="text"
+                        />
+                    </InputGroup>
+                    <Button className="submit" onClick={handleSubmit} variant='primary'>Submit</Button>
 
-                <input
-                    type="text"
-                    name="email"
-                    onChange={handleChange}
-                    placeholder="Email"
-                    value={details.email}
-                />
-                <br />
-                <button className="update-user-button">Update</button>
-            </form>
+
+                </form>
+            </Card>
         </div>
     )
 }
