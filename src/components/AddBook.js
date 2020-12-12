e2abd72bdfe3e34cd88f4c637183df01cd160163
meloegel <meloegel@gmail.com>
2020-12-11 21:17:59 -0500

    adding react bootstrap to add book

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -3,6 +3,11 @@ import { useHistory } from "react-router-dom";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import BookContext from '../contexts/BookContext'
 import UserContext from '../contexts/UserContext'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
 
 const initlalDetails = {
     title: '',
@@ -41,70 +46,72 @@ const AddBook = () => {
 
     return (
         <div id='addBook'>
-            <h2>Add A Book</h2>
-            <form id='addBookForm' onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="title"
-                    onChange={handleChange}
-                    placeholder="Title"
-                    value={details.title}
-                />
-                <div />
-                <input
-                    type="text"
-                    name="author"
-                    onChange={handleChange}
-                    placeholder="Author"
-                    value={details.author}
-                />
-                <div />
-                <input
-                    type="text"
-                    name="pageCount"
-                    onChange={handleChange}
-                    placeholder="Page Count"
-                    value={details.pageCount}
-                />
-                <div />
-                <input
-                    type="text"
-                    name="yearPublished"
-                    onChange={handleChange}
-                    placeholder="Year Published"
-                    value={details.yearPublished}
-                />
-                <div />
-                <div />
-                <input
-                    type="text"
-                    name="coverArt"
-                    onChange={handleChange}
-                    placeholder="Cover Art"
-                    value={details.coverArt}
-                />
-                <div />
-                <div />
-                <input
-                    type="text"
-                    name="description"
-                    onChange={handleChange}
-                    placeholder="Description"
-                    value={details.description}
-                />
-                <div />
-                <div />
-                <input
-                    id='idInput'
-                    type="text"
-                    name="userId"
-                    onChange={handleChange}
-                    value={userId.userId}
-                />
-                <div />
-                <br />
-                <button className="add-button">Add New Book</button>
-            </form>
+            <Card body bg='info' className='formCardReg'>
+                <h2>Add A Book</h2>
+                <form id='addBookForm' onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="title"
+                        onChange={handleChange}
+                        placeholder="Title"
+                        value={details.title}
+                    />
+                    <div />
+                    <input
+                        type="text"
+                        name="author"
+                        onChange={handleChange}
+                        placeholder="Author"
+                        value={details.author}
+                    />
+                    <div />
+                    <input
+                        type="text"
+                        name="pageCount"
+                        onChange={handleChange}
+                        placeholder="Page Count"
+                        value={details.pageCount}
+                    />
+                    <div />
+                    <input
+                        type="text"
+                        name="yearPublished"
+                        onChange={handleChange}
+                        placeholder="Year Published"
+                        value={details.yearPublished}
+                    />
+                    <div />
+                    <div />
+                    <input
+                        type="text"
+                        name="coverArt"
+                        onChange={handleChange}
+                        placeholder="Cover Art"
+                        value={details.coverArt}
+                    />
+                    <div />
+                    <div />
+                    <input
+                        type="text"
+                        name="description"
+                        onChange={handleChange}
+                        placeholder="Description"
+                        value={details.description}
+                    />
+                    <div />
+                    <div />
+                    <input
+                        id='idInput'
+                        type="text"
+                        name="userId"
+                        onChange={handleChange}
+                        value={userId.userId}
+                    />
+                    <div />
+                    <br />
+                    <button className="add-button">Add New Book</button>
+                </form>
+            </Card>
         </div>
     )
 }
